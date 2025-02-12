c033fa3e1ccefff523a1bb4c49a34214411461f3
Adam Bloom <adam-bloom@users.noreply.github.com>
2025-02-11 20:57:58 -0700

    fix: font setup

--- setup.sh
+++ setup.sh
@@ -35,14 +35,14 @@ if [ ! -d "$ZSH_PLUGIN_AUTOSUGGEST_DIR" ]; then
 fi
 
 # install fonts
-curl -fsSL -o "$HOME/Library/Fonts/DejaVu Sans Mono Nerd Font Complete Mono.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete%20Mono.ttf
-curl -fsSL -o "$HOME/Library/Fonts/DejaVu Sans Mono Nerd Font Complete.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete.ttf
-curl -fsSL -o "$HOME/Library/Fonts/DejaVu Sans Mono Oblique Nerd Font Complete Mono.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DejaVuSansMono/Italic/complete/DejaVu%20Sans%20Mono%20Oblique%20Nerd%20Font%20Complete%20Mono.ttf
-curl -fsSL -o "$HOME/Library/Fonts/DejaVu Sans Mono Oblique Nerd Font Complete.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DejaVuSansMono/Italic/complete/DejaVu%20Sans%20Mono%20Oblique%20Nerd%20Font%20Complete.ttf
-curl -fsSL -o "$HOME/Library/Fonts/DejaVu Sans Mono Bold Nerd Font Complete Mono.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DejaVuSansMono/Bold/complete/DejaVu%20Sans%20Mono%20Bold%20Nerd%20Font%20Complete%20Mono.ttf
-curl -fsSL -o "$HOME/Library/Fonts/DejaVu Sans Mono Bold Nerd Font Complete.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DejaVuSansMono/Bold/complete/DejaVu%20Sans%20Mono%20Bold%20Nerd%20Font%20Complete.ttf
-curl -fsSL -o "$HOME/Library/Fonts/DejaVu Sans Mono Bold Oblique Nerd Font Complete Mono.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DejaVuSansMono/Bold-Italic/complete/DejaVu%20Sans%20Mono%20Bold%20Oblique%20Nerd%20Font%20Complete%20Mono.ttf
-curl -fsSL -o "$HOME/Library/Fonts/DejaVu Sans Mono Bold Oblique Nerd Font Complete.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DejaVuSansMono/Bold-Italic/complete/DejaVu%20Sans%20Mono%20Bold%20Oblique%20Nerd%20Font%20Complete.ttf
+font_dir=$(mktemp -d)
+pushd .
+cd $font_dir
+curl -fsSL -o ./dejavu.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/DejaVuSansMono.zip
+unzip ./dejavu.zip
+cp ./*.ttf "$HOME/Library/Fonts/."
+popd
+rm -r $font_dir
 
 # setup zsh
 ln -sf "$(pwd)/zsh/zshrc" "$HOME/.zshrc"
